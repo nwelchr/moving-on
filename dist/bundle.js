c1142a3f66761937551273f5b60f26eda1f4b26a
nwelchr <nwelchr@gmail.com>
2018-05-11 01:15:40 -0700

    Being abstracting out entry file into separate class

--- dist/bundle.js
+++ dist/bundle.js
@@ -452,6 +452,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
+// KEYCODES
+
 const keyCodes = {
     37: 'left',
     38: 'up',
@@ -459,25 +461,33 @@ const keyCodes = {
     83: 'switch'
 };
 
+// MUSIC
+
 const audio = document.getElementById('intro');
 let musicIsPlaying = false;
 const finish = document.getElementById('level-finish');
 audio.volume = 0.2;
 audio.loop = true;
 finish.volume = 0.05;
+
+// MODALS / BUTTONS
+
 const pauseModal = document.querySelector('.pause-modal');
 const pauseButton = document.querySelector('.unpause');
 const restartButton = document.querySelector('.restart');
 const titleScreen = document.querySelector('.title-screen');
 const startButton = document.querySelector('.start');
 
+// GAME
+
+let gameIsRunning = true;
+
 const detectKeys = () => {
     // to avoid error with indexing into something that doesn't exist
     const isPressed = Object.create(null);
 
     const track = e => {
-        // if (e.keyCode === 27) togglePauseScreen(e);
-        if (keyCodes.hasOwnProperty(e.keyCode)) {
+        if (e.keyCode === 27) togglePauseScreen(e);else if (keyCodes.hasOwnProperty(e.keyCode)) {
             e.preventDefault();
             const isKeydown = e.type === 'keydown';
             isPressed[keyCodes[e.keyCode]] = isKeydown;
@@ -487,58 +497,107 @@ const detectKeys = () => {
     window.addEventListener('keydown', track);
     window.addEventListener('keyup', track);
 
-    console.log(isPressed, "his");
     return isPressed;
 };
 
-let gameIsRunning = true;
+const startGame = () => {
+    musicIsPlaying = true;
+    audio.play();
 
-// const togglePauseScreen = (e) => {
-//     // const didPressEsc = e.keyCode === 27;
-//     // const didClick = e.type === "click";
-//         gameIsRunning = !gameIsRunning;
+    titleScreen.classList.remove('show');
+    startLevel(0);
+};
 
-//         // toggle music
-//         musicIsPlaying = !musicIsPlaying;
-//         musicIsPlaying ? audio.play() : audio.pause();
+const startLevel = levelId => {
 
+    const statusFunction = status => {
+        if (status.includes('lost')) {
+            startLevel(levelId);
+        } else if (levelId < __WEBPACK_IMPORTED_MODULE_2__level_maps__["a" /* default */].length - 1) {
+            startLevel(levelId + 1);
+        } else {
+            titleScreen.classList.add('show');
+        }
+    };
 
-//         pauseModal.classList.toggle("show");
-//         if (gameIsRunning) runAnimation()
-// };
+    runLevel(new __WEBPACK_IMPORTED_MODULE_0__level__["a" /* default */](__WEBPACK_IMPORTED_MODULE_2__level_maps__["a" /* default */][levelId], levelId + 1), statusFunction);
+};
 
-// pauseButton.addEventListener('click', togglePauseScreen);
+const runLevel = (level, statusFunction) => {
+    const gameWrapper = document.getElementById('game-wrapper');
+    const display = new __WEBPACK_IMPORTED_MODULE_1__display__["a" /* default */](gameWrapper, level);
+    let state = __WEBPACK_IMPORTED_MODULE_3__state__["a" /* default */].start(level);
+    let ending = 1;
+
+    // Rotate on the 10th level after 10 seconds
+    if (level.levelId === 10) {
+        setTimeout(rotate, 10000);
+    } else {
+        if (gameWrapper.classList.contains('rotated')) {
+            gameWrapper.classList.remove('rotated');
+        }
+    }
+
+    const rotate = () => {
+        const wrap = document.getElementById('game-wrapper');
+        wrap.classList.add('rotated');
+    };
+
+    console.log('hi');
+
+    restartButton.addEventListener('click', e => {
+        e.preventDefault();
+        gameIsRunning = !gameIsRunning;
+        pauseModal.classList.toggle("show");
+
+        musicIsPlaying = false;
+        audio.pause();
+
+        ending = 0;
+
+        display.clear('restart button clicked', state.status);
+        statusFunction('lost');
+        return;
+    });
+
+    const frameFunction = time => {
+        state = state.update(time, keys);
+        display.drawFrame(state);
+
+        if (state.status.includes('playing')) {
+            return true;
+        } else if (ending > 0) {
+            finish.play();
+            ending -= time;
+            return true;
+        } else {
+            display.clear('else statement of runAnimation', state.status);
+            statusFunction(state.status);
+            return false;
+        }
+    };
+
+    runAnimation(frameFunction);
+};
+
+const togglePauseScreen = e => {
+    // const didPressEsc = e.keyCode === 27;
+    // const didClick = e.type === "click";
+    gameIsRunning = !gameIsRunning;
+
+    // toggle music
+    musicIsPlaying = !musicIsPlaying;
+    musicIsPlaying ? audio.play() : audio.pause();
+
+    pauseModal.classList.toggle("show");
+};
+
+pauseButton.addEventListener('click', togglePauseScreen);
 
 // calls requestAnimation again after every frame
 const runAnimation = frameFunction => {
     // last time since window has been open
     let lastTime = null;
-
-    window.addEventListener('keydown', e => {
-        if (e.keyCode === 27) {
-            // toggle gameplay
-            gameIsRunning = !gameIsRunning;
-
-            // toggle music
-            musicIsPlaying = !musicIsPlaying;
-            musicIsPlaying ? audio.play() : audio.pause();
-
-            pauseModal.classList.toggle("show");
-        }
-        if (gameIsRunning) requestAnimationFrame(frame);
-    });
-
-    // pauseButton.addEventListener('click', (e) => {
-    //     debugger;
-    //     e.preventDefault();
-    //     gameIsRunning = !gameIsRunning;
-    //     pauseModal.classList.toggle("show");
-
-    //     musicIsPlaying = true;
-    //     audio.play();
-
-    //     if (gameIsRunning) requestAnimationFrame(frame);
-    // });
 
     const frame = time => {
         if (gameIsRunning === false) {
@@ -560,86 +619,6 @@ const runAnimation = frameFunction => {
 
 // let nextLevelCount = 0;
 
-const runLevel = (level, successFunction) => {
-    const gameWrapper = document.getElementById('game-wrapper');
-    const display = new __WEBPACK_IMPORTED_MODULE_1__display__["a" /* default */](gameWrapper, level);
-    let state = __WEBPACK_IMPORTED_MODULE_3__state__["a" /* default */].start(level);
-    let ending = 1;
-
-    // Rotate on the 10th level after 10 seconds
-    if (level.levelId === 10) {
-        setTimeout(rotate, 10000);
-    } else {
-        if (gameWrapper.classList.contains('rotated')) {
-            gameWrapper.classList.remove('rotated');
-        }
-    }
-
-    const frameFunction = time => {
-        state = state.update(time, keys);
-        display.drawFrame(state);
-
-        restartButton.addEventListener('click', e => {
-            e.preventDefault();
-            gameIsRunning = !gameIsRunning;
-            pauseModal.classList.toggle("show");
-
-            musicIsPlaying = false;
-            audio.pause();
-
-            display.clear('restart button clicked', state.status);
-            successFunction('lost');
-            return;
-        });
-
-        if (state.status.includes('playing')) {
-            return true;
-        } else if (ending > 0) {
-            finish.play();
-            ending -= time;
-            return true;
-        } else {
-            // if (nextLevelCount === 0) {
-            // nextLevelCount++;
-            // }
-            display.clear('else statement of runAnimation', state.status);
-            successFunction(state.status);
-            return false;
-        }
-    };
-
-    runAnimation(frameFunction);
-};
-
-const rotate = () => {
-    const wrap = document.getElementById('game-wrapper');
-    wrap.classList.add('rotated');
-};
-
-const startLevel = n => {
-
-    const statusFunction = status => {
-        if (status.includes('lost')) {
-            startLevel(n);
-        } else if (n < __WEBPACK_IMPORTED_MODULE_2__level_maps__["a" /* default */].length - 1) {
-            startLevel(n + 1);
-        } else {
-            titleScreen.classList.add('show');
-        }
-    };
-
-    runLevel(new __WEBPACK_IMPORTED_MODULE_0__level__["a" /* default */](__WEBPACK_IMPORTED_MODULE_2__level_maps__["a" /* default */][n], n + 1), statusFunction);
-};
-
-// conditions to start game
-
-const startGame = () => {
-    musicIsPlaying = true;
-    audio.play();
-
-    titleScreen.classList.remove('show');
-    startLevel(0);
-};
 
 startButton.addEventListener('click', () => startGame());
 
--- javascripts/movingOn.js
+++ javascripts/movingOn.js
@@ -24,20 +24,125 @@ const restartButton = document.querySelector('.restart');
 const titleScreen = document.querySelector('.title-screen');
 const startButton = document.querySelector('.start');
 
-const detectKeys = () => {
-    // to avoid error with indexing into something that doesn't exist
-    const isPressed = Object.create(null);
+const gameWrapper = document.getElementById('game-wrapper');
 
-    const track = (e) => {
+class Game {
+    constructor() {
+        this.musicIsPlaying = false;
+        this.gameIsRunning = true;
+        this.keys = Object.create(null);
+        this.levelId = 0;
+    }
+
+    trackKeys(e) {
         if (keyCodes.hasOwnProperty(e.keyCode)) {
             e.preventDefault();
             const isKeydown = (e.type === 'keydown');
-            isPressed[keyCodes[e.keyCode]] = isKeydown;
+            this.keys[keyCodes[e.keyCode]] = isKeydown;
         }
-    };
+    }
 
-    window.addEventListener('keydown', track);
-    window.addEventListener('keyup', track);
+    startGame() {
+        this.musicIsPlaying = true;
+        audio.play();
     
-    return isPressed;
-};
+        titleScreen.classList.remove('show');
+        this.startLevel(0);
+    }
+
+    startLevel() {
+        this.runLevel(new Level(levelMaps[this.levelId], this.levelId + 1), this.statusFunction);
+    }
+
+    statusFunction (status) {
+        if (status.includes('lost')) {
+            this.startLevel();
+        } else if (this.levelId < levelMaps.length - 1) {
+            this.levelId += 1;
+            this.startLevel();
+        } else {
+            this.titleScreen.classList.add('show');
+        }
+    }
+
+    runLevel (level, statusFunction) {
+        const display = new Display(gameWrapper, level);
+        let state = State.start(level);
+        let ending = 1;
+    
+        // Rotate on the 10th level after 10 seconds
+
+        if (level.levelId === 10) {
+            setTimeout(rotate, 10000);
+        } else if (gameWrapper.classList.contains('rotated')) {
+                gameWrapper.classList.remove('rotated');
+        }
+    
+        const rotate = () => {
+            const wrap = document.getElementById('game-wrapper');
+            wrap.classList.add('rotated');
+        };
+    
+        restartButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.gameIsRunning = !this.gameIsRunning;
+            pauseModal.classList.toggle("show");
+    
+            this.musicIsPlaying = false;
+            audio.pause();
+    
+            ending = 0;
+    
+            display.clear('restart button clicked', state.status);
+            statusFunction('lost');
+            return;
+        });
+    
+        const frameFunction = time => {
+            state = state.update(time, this.keys);
+            display.drawFrame(state);
+    
+            if (state.status.includes('playing')) {
+                return true;
+            } else if (ending > 0) {
+                finish.play();
+                ending -= time;
+                return true;
+            } else {
+                display.clear('else statement of runAnimation', state.status);
+                statusFunction(state.status);
+                return false;
+            }
+        };
+    
+    
+        this.runAnimation(frameFunction);
+    }
+
+    runAnimation (frameFunction) {
+        // last time since window has been open
+        let lastTime = null;
+    
+        const frame = (time) => {
+            if (this.gameIsRunning === false) {
+                return;
+            }
+    
+            if (lastTime !== null) {
+                // converts time between ms and s for convenience
+                let timeStep = Math.min(time - lastTime, 100) / 1000;
+                if (frameFunction(timeStep) === false) return;
+            }
+    
+            lastTime = time;
+            requestAnimationFrame(frame);
+        };
+    
+        if (this.gameIsRunning) requestAnimationFrame(frame);
+    }
+}
+
+
+window.addEventListener('keydown', Game.trackKeys);
+window.addEventListener('keyup', Game.trackKeys);
+
